7e4cdf4874be305a95d57838f7c6f2594c580eea
Shyam Barua <138357317+Shyambarua@users.noreply.github.com>
2024-05-09 12:23:37 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 // 1. Middleware this line of code is used to parse the data from the body of the request
 
 const corsOptions = {
-  origin: "e-commerce-orpin-one.vercel.app",
+  origin: "e-commerce-1j5a.vercel.app",
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   credentials: true,
 };
