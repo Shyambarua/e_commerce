89853c4b7e913dbe911b016a88428d196dd09c8c
Shyam Barua <138357317+Shyambarua@users.noreply.github.com>
2024-05-09 12:18:51 +0530

    LoginForm.jsx

--- Buyzy/src/components/LoginForm.jsx
+++ Buyzy/src/components/LoginForm.jsx
@@ -18,7 +18,7 @@ const LoginForm = () => {
     event.preventDefault();
     console.log(user);
     try {
-      const response = await fetch("http://localhost:5000/api/auth/register", {
+      const response = await fetch("e-commerce-orpin-one.vercel.app/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
